937909724dcd33ed85813049097519df8ead35ef
Jerr1AJ <jamsjunyor@hotmail.com>
2024-10-18 21:54:26 -0300

    Create deposito-banco.py

---
+++ bootcamp-nttdados/deposito-banco.py
@@ -0,0 +1,99 @@
+import time
+
+saldo = 0
+deposito = 0
+saque = float(0)
+limite_saques = 3
+numero_saques = 0
+extrato = ""
+
+def menu():
+    print(    
+        "---===---===---===---\n"
+        "      SEU BANCO   \n"
+        "---===---===---===---\n"
+        "Escolha uma opção:\n"
+        "[1] Depositar\n"
+        "[2] Sacar\n"
+        "[3] Extrato\n"
+        "[0] Sair\n"   
+        "---===---===---===---\n"     
+    )
+
+    print('R${:.2f}'.format(saldo))
+
+    opção = int(input('Qual opção:'))
+
+    if opção == 1:
+        depositar()
+    elif opção == 2:
+        sacar()
+    elif opção == 3:
+        ver_extrado()
+    elif opção == 0:
+        print("Muito obrigado por usar o SEUBANCO, até mais!")
+    else:
+        print("Opção inválida, digite novamente!")
+        #time.sleep(3)
+        menu()
+
+
+def depositar():
+    global saldo
+    global extrato
+
+    deposito = float(input("Qual o valor do deposito: R$ "))
+
+    if deposito > 0:
+        saldo += deposito
+        extrato = f"Deposito: R$ {deposito:.2f}\n"
+
+    else:
+        print("O valor indicado é inválido, tente novamente!")
+        depositar()    
+
+    #print('R${:.2f}'.format(saldo))
+    print("Seu deposito foi confirmado!")
+    #time.sleep(3)
+    menu()
+
+
+def sacar():
+    global saldo
+    global saque
+    global extrato
+    global numero_saques
+
+    if numero_saques == limite_saques:
+        print("Limite de saques diários alcançados! Retornando ao menu")
+        time.sleep(2)
+        menu()
+
+    else:
+        print(
+            "---===---===---===---\n"
+            "Menu de Saque\n"
+            "\n"
+            )
+        saque = float(input("Quanto você deseja sacar: R$ "))
+        if saque > saldo:
+            print("O valor do Saque é maior que o Saldo, tente novamente!")
+            sacar()
+        
+        else:
+            saldo -= saque
+            numero_saques += 1
+            extrato += f"Saque nº{numero_saques}: R$ {saque:.2f}\n"
+            print("Seu Saque foi confirmado!")
+            #time.sleep(3)
+            menu()
+
+
+def ver_extrado():
+    print(extrato)
+    print("========")
+    print(f"Saldo: R$ {saldo:.2f}")
+    #time.sleep(2)
+    menu()
+
+menu()
